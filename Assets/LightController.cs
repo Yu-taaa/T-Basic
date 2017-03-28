51971a3d4374652999361f0dc36d72015c0439b9
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-03-28 21:36:59 +0900

    new

---
+++ Assets/LightController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > 3.0f) {
+			transform.rotation = Quaternion.Euler (Time.time * 30.0f, 0, 0);
+		}
+
+		if (Time.time > 20.0f) {
+			transform.rotation = Quaternion.Euler (50.0f, -30.0f, 0);
+		}
+	}
+}
